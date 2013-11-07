950afd5748f01668d0d95cd3f324e46327c642e2
duncangarde <duncangarde@hotmail.com>
2013-11-07 14:38:55 +0000

    completed user model

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,6 +6,7 @@ describe "User Pages" do
   	before { visit signup_path }
     it { should have_content('Sign Up')}
     it { should have_title(full_title('Sign Up')) }
-      
+    
+   
     end
   end
