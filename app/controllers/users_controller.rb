7590c63f4e9ea54071d89333335059e6fafd9a97
duncangarde <duncangarde@hotmail.com>
2013-11-21 11:52:09 +0000

    finished user model actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
-  	def new
+  before_action :signed_in_user, only: [:edit, :update, :index, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+    def new
   		@user = User.new
   	end
 
@@ -8,7 +11,7 @@ class UsersController < ApplicationController
 	end
 
 	def create
-    @user = User.new(user_params)    
+  @user = User.new(user_params)    
     	if @user.save
         sign_in @user
     		flash[:success] = "Welcome to the Sample App!"
@@ -16,7 +19,29 @@ class UsersController < ApplicationController
    		 else
     	  render 'new'
     	end
-  	end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
 
   private
 
@@ -25,5 +50,21 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
+    # Before filters
 
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
