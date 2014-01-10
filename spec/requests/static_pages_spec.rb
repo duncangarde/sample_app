71d73056983ce99ade263994af8397ba761c0955
duncangarde <duncangarde@hotmail.com>
2014-01-10 12:43:39 +0000

    adding microposts and feed

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "Static pages" do
   subject {page}
 
-shared_examples_for "all static pages" do
-  it { should have_selector('h1', text: heading) }
-  it {should have_title(full_title(page_title)) }
-end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it {should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
@@ -15,6 +15,25 @@ end
 
     it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
+
+    describe "for signed in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed with delete links only on own posts" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+          if item.user != user
+          expect(page).not_to have_link('delete')
+          end
+        end
+      end
+    end   
   end
 
 
@@ -26,7 +45,7 @@ end
     it_should_behave_like "all static pages"
   end
 
-describe "About page" do
+  describe "About page" do
     before {visit about_path}
     let(:heading) { 'About Us'}
     let(:page_title) {'About Us'}
@@ -34,7 +53,7 @@ describe "About page" do
     it_should_behave_like "all static pages"
   end
     
-describe "Help page" do
+  describe "Help page" do
     before {visit help_path}
     let(:heading) { 'Help'}
     let(:page_title) {'Help'}
@@ -42,20 +61,19 @@ describe "Help page" do
     it_should_behave_like "all static pages"
   end
 
-it "should have the correct links" do
-  visit root_path
-  click_link "Sign up now!"
-  expect(page).to have_title(full_title('Sign Up'))
-  click_link "Home"
-  expect(page).to have_title(full_title(''))
-  click_link "About"
-  expect(page).to have_title(full_title('About Us'))
-  click_link "Contact"
-  expect(page).to have_title(full_title('Contact Us'))
-  click_link "Help"
-  expect(page).to have_title(full_title('Help'))
-  click_link "sample app"
-  expect(page).to have_title(full_title(''))
-end
-
+  it "should have the correct links" do
+    visit root_path
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "Home"
+    expect(page).to have_title(full_title(''))
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
+  end
 end
